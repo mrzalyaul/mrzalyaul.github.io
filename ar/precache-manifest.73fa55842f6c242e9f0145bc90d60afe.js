1bce22f4cba2ce222e62f703212678479e740110
ZALYAUL Mohammed Rabie <mrzalyaul@gmail.com>
2020-08-22 18:44:39 +0100

    Update the arabic website add pwa

---
+++ ar/precache-manifest.73fa55842f6c242e9f0145bc90d60afe.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c22da15ea6b2a0471f14d85e06a2adca",
+    "url": "./index.html"
+  },
+  {
+    "revision": "fea69138c37bd8762612",
+    "url": "./static/css/2.07eacbe5.chunk.css"
+  },
+  {
+    "revision": "fd68f084377b8dda7306",
+    "url": "./static/css/main.ce90aa18.chunk.css"
+  },
+  {
+    "revision": "fea69138c37bd8762612",
+    "url": "./static/js/2.11728d7c.chunk.js"
+  },
+  {
+    "revision": "851dde5cb57391f99eae94821a738a11",
+    "url": "./static/js/2.11728d7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd68f084377b8dda7306",
+    "url": "./static/js/main.1541a011.chunk.js"
+  },
+  {
+    "revision": "61010ed31aea2bc82ab7",
+    "url": "./static/js/runtime-main.70647feb.js"
+  },
+  {
+    "revision": "378ee291669883b443cc8036d148b4fe",
+    "url": "./static/media/JannaLTRegular.378ee291.ttf"
+  }
+]);
