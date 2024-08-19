36b94740432817eccaf802b60950f2af22a59dcf
ZALYAUL Mohammed Rabie <mrzalyaul@gmail.com>
2024-08-19 23:35:29 +0200

    feat: update the contact page description

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -2,7 +2,7 @@ export default function Page() {
     return (
         <div>
             <h1>Contact</h1>
-            <p>This is an example of a page.</p>
+            <p>This is the contact page.</p>
         </div>
     );
 }
